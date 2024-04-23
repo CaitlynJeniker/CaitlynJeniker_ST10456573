cd294d3826efa5b5bd13bd1c1b456f832b666688
CaitlynJeniker <ST10456573@vcconnect.edu.za>
2024-04-23 15:31:59 +0200

    not done

---
+++ app/src/main/java/com/example/caitlynjeniker_a2_st10456573/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.caitlynjeniker_a2_st10456573
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/caitlynjeniker_a2_st10456573/secondPage.kt
@@ -0,0 +1,12 @@
+package com.example.caitlynjeniker_a2_st10456573
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class secondPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_page)
+
+    }
+}
