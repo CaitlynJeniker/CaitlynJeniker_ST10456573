30156b0ef2cfa9b5a3dedb318e9643a95e7c806c
CaitlynJeniker <ST10456573@vcconnect.edu.za>
2024-05-06 11:03:56 +0200

    DONE

--- app/src/main/java/com/example/caitlynjeniker_a2_st10456573/MainActivity.kt
+++ app/src/main/java/com/example/caitlynjeniker_a2_st10456573/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.caitlynjeniker_a2_st10456573
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+            val intent = Intent(this, secondPage::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/caitlynjeniker_a2_st10456573/secondPage.kt
+++ app/src/main/java/com/example/caitlynjeniker_a2_st10456573/secondPage.kt
@@ -2,11 +2,54 @@ package com.example.caitlynjeniker_a2_st10456573
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class secondPage : AppCompatActivity() {
+
+    lateinit var image: ImageView
+    lateinit var feedCountTextView: TextView
+    lateinit var playCountTextView: TextView
+    lateinit var cleanCountTextView: TextView
+     var feedClickCount: Int = 0
+     var playClickCount: Int = 0
+     var cleanclickCount: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_page)
 
+        image = findViewById(R.id.doggy)
+
+
+
+        feedCountTextView = findViewById(R.id.feedCount)
+        playCountTextView = findViewById(R.id.playCount)
+        cleanCountTextView = findViewById(R.id.cleanCount)
+
+
+        val FeedBtn = findViewById<Button>(R.id.FeedBtn)
+        FeedBtn.setOnClickListener {
+            image.setImageResource(R.drawable.dog_eating)
+            feedClickCount++
+            feedCountTextView.text = "$feedClickCount"
+
+        }
+        val PlayBtn = findViewById<Button>(R.id.PlayBtn)
+        PlayBtn.setOnClickListener {
+            image.setImageResource(R.drawable.dog_playing)
+            playClickCount++
+            playCountTextView.text = "$playClickCount"
+        }
+        val CleanBtn = findViewById<Button>(R.id.CleanBtn)
+        CleanBtn.setOnClickListener {
+            image.setImageResource(R.drawable.dog_washing)
+            cleanclickCount++
+            cleanCountTextView.text = "$cleanclickCount"
+        }
     }
 }
+
+
+
